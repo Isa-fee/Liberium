90e41f2af32587a0b6904303a523a0ba54d86988
Isa-fee <tiagosoaresjp1995@gmail.com>
2025-10-24 17:38:00 -0300

    criação dos arquivos das principais páginas, ainda sem conteúdo

--- app.py
+++ app.py
@@ -5,5 +5,26 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+   
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
-    
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/books')
+def books():
+    return render_template('books.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
