35ca1558e0f6937ba3735f5e8234800977f87ad3
Isa-fee <tiagosoaresjp1995@gmail.com>
2025-11-18 19:22:22 -0300

    criação da página perfil

--- app.py
+++ app.py
@@ -26,5 +26,9 @@ def books():
 def sobre():
     return render_template('sobre.html')
 
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
