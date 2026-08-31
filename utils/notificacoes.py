b4599d14390cad618941fd0627512a8cac9eb5d4
medeirostamiris <tamirismdrs99@gmail.com>
2026-08-31 14:32:35 -0300

    implementação de notificações inicialmente

---
+++ utils/notificacoes.py
@@ -0,0 +1,25 @@
+from extensions import db
+from models import Notificacao
+
+
+def criar_notificacao(
+    usuario_id,
+    categoria,
+    tipo,
+    titulo,
+    mensagem,
+    link=None
+):
+    notificacao = Notificacao(
+        usuario_id=usuario_id,
+        categoria=categoria,
+        tipo=tipo,
+        titulo=titulo,
+        mensagem=mensagem,
+        link=link
+    )
+
+    db.session.add(notificacao)
+    db.session.commit()
+
+    return notificacao
